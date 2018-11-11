cc0cf1444cb7810a3b5851fb4779656a3c107f7f
diogommartins <magalhaesmartins@icloud.com>
2018-11-11 20:05:17 -0200

    Adding example application with http server and health-checker

---
+++ examples/with_http_server.py
@@ -0,0 +1,63 @@
+import asyncio
+from typing import List
+
+from aiohttp import web
+
+from asyncworker import App, Options
+from asyncworker.rabbitmq import RabbitMQMessage
+
+try:
+    from healthchecker import check
+    from healthchecker.async_checker import AsyncCheckCase
+    HEALTHCHECKER_ENABLED = True
+except ImportError:
+    HEALTHCHECKER_ENABLED = False
+
+
+__doc__ = """
+Run me with ASYNCWORKER_HTTP_ENABLED=1 python examples/with_http_server.py
+Then access http://localhost:8080/ or http://localhost:8080/healthcheck (if you 
+have "health-checker" package installed)
+"""
+
+app = App(host="localhost", user="guest", password="guest", prefetch_count=1024)
+
+
+@app.http_route('GET', '/')
+async def index(request: web.Request) -> web.Response:
+    return web.Response(body="Hello world")
+
+
+if HEALTHCHECKER_ENABLED:
+    @app.http_route('GET', '/healthcheck')
+    class HealthCheck(AsyncCheckCase, web.View):
+        @property
+        def loop(self) -> asyncio.AbstractEventLoop:
+            return self.request.app.loop
+
+        async def get(self) -> 'web.Response':
+            """
+            Should return 200 if all dependencies are ok, 500 otherwise.
+            :returns: A HTTP response with True or False for each check
+            """
+            await self.check()
+
+            status_code = 200 if self.has_succeeded() else 500
+
+            return web.json_response(data=self.check_report, status=status_code)
+
+        @check
+        async def validate_something(self):
+            return True
+
+        @check
+        async def validate_something_else(self):
+            return False
+
+
+@app.route(["words_to_index"], vhost="/", options={Options.BULK_SIZE: 512})
+async def drain_handler(messages: List[RabbitMQMessage]):
+    print(messages)
+
+
+app.run()
