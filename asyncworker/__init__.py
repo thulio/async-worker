4db6102a048a2aab31b32a48882cba6a007f5efc
diogommartins <magalhaesmartins@icloud.com>
2018-11-11 18:12:33 -0200

    Moving BaseApp to asyncworker.base

--- asyncworker/__init__.py
+++ asyncworker/__init__.py
@@ -1,59 +1,6 @@
-import functools
-import asyncio
-from collections import UserDict
-from typing import Iterable, Callable, Coroutine, Dict, Optional
-
-from .consumer import Consumer
-
-from asyncworker import conf
+from asyncworker.base import BaseApp
+from asyncworker.consumer import Consumer
 from asyncworker.options import Options, Defaultvalues, Events
-from .bucket import Bucket
-
-
-def entrypoint(f):
-    @functools.wraps(f)
-    def _(*args, **kwargs):
-        loop = asyncio.get_event_loop()
-        return loop.run_until_complete(f(*args, **kwargs))
-    return _
-
-class BaseApp:
-    def __init__(self) -> None:
-        self.loop = asyncio.get_event_loop()
-        self.routes_registry: Routes[Route, Dict] = {}
-
-    def _build_consumers(self):
-        raise NotImplementedError()
-
-    @entrypoint
-    async def run(self) -> None:
-        conf.logger.info("Booting App...")
-        self._on_startup.freeze()
-        await self.startup()
-        consumers = self._build_consumers()
-        for consumer in consumers:
-            self.loop.create_task(consumer.start())
-        while True:
-            await asyncio.sleep(10)
-
-    async def startup(self) -> None:
-        """Causes on_startup signal
-
-        Should be called in the event loop along with the request handler.
-        """
-        await self._on_startup.send(self)
-
-    async def start_http_server(self, app: 'BaseApp'):
-        app.http_app = web.Application()
-        for func, route in app.routes_registry.http_routes().item():
-            app.http_app.add_routes(route)
-
-        self.http_runner = web.AppRunner(app.http_app)
-        await self.http_runner.setup()
-        site = web.TCPSite(self.http_runner, )
-
-    async def stop_http_server(self, app: 'BaseApp'):
-        await app.http_runner.cleanup()
 
 
 class App(BaseApp):
---
+++ asyncworker/base.py
@@ -0,0 +1,41 @@
+import asyncio
+from typing import Optional, Dict
+
+from aiohttp import web, Signal
+
+from asyncworker.conf import settings, logger
+from asyncworker.http_server import HTTPServer
+from asyncworker.models import Routes, Route
+from asyncworker.utils import entrypoint
+
+
+class BaseApp:
+    http_app: Optional[web.Application]
+    http_runner: Optional[web.AppRunner]
+
+    def __init__(self) -> None:
+        self.loop = asyncio.get_event_loop()
+        self.routes_registry: Routes[Route, Dict] = Routes()
+
+        self._on_startup = Signal(self)
+
+    def _build_consumers(self):
+        raise NotImplementedError()
+
+    @entrypoint
+    async def run(self) -> None:
+        logger.info("Booting App...")
+        self._on_startup.freeze()
+        await self.startup()
+        consumers = self._build_consumers()
+        for consumer in consumers:
+            self.loop.create_task(consumer.start())
+        while True:
+            await asyncio.sleep(10)
+
+    async def startup(self) -> None:
+        """Causes on_startup signal
+
+        Should be called in the event loop along with the request handler.
+        """
+        await self._on_startup.send(self)
