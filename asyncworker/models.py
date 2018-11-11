a08155eb6ec198e6e12f7095454bff8879f6c920
diogommartins <magalhaesmartins@icloud.com>
2018-11-11 18:13:12 -0200

    Adding Routes container

---
+++ asyncworker/models.py
@@ -0,0 +1,34 @@
+from collections import UserDict
+from typing import Callable, Coroutine, Dict
+
+from yarl import cached_property
+
+
+Route = Callable[[], Coroutine]
+
+
+class Routes(UserDict):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._cache = {}
+
+    @cached_property
+    def http_routes(self) -> Dict[Route, Dict]:
+        routes = {}
+        for handler, route in self.items():
+            if route['type'] == 'http':
+                routes[handler] = {
+                    'method': route['method'],
+                    'path': route['path'],
+                    'handler': handler
+                }
+        return routes
+
+    @cached_property
+    def amqp_routes(self) -> Dict[Route, Dict]:
+        routes = {}
+        for handler, route in self.items():
+            if route['type'] == 'amqp':
+                routes[handler] = dict(**route)
+                routes[handler].pop('type')
+        return routes
